d2be396d00e1683a64c3bb50d68b46785957a928
Yogesh Shivacharya <yogeshshivacharya@rarecrew.com>
2024-11-25 12:46:51 +0530

    Ticket #Auth-001: RELEASE: Initial commit

---
+++ DataingApp/API/Controllers/AccountController.cs
@@ -0,0 +1,71 @@
+﻿using API.Data;
+using API.Dtos;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController: BaseApiController
+    {
+        public readonly DataContext _dataContext;
+        private readonly ITokenService tokenService;
+
+        public AccountController(DataContext dataContext,ITokenService tokenService)
+        {
+            this._dataContext = dataContext;
+            this.tokenService = tokenService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register([FromBody]RegisterDto register)
+        {
+            var userexist = await _dataContext.User.AnyAsync(x => x.UserName == register.UserName.ToLower());
+            if (userexist)
+            {
+                return BadRequest();
+            }
+            var hmac = new HMACSHA512();
+            var user = new AppUser
+            {
+                UserName = register.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(register.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _dataContext.Add(user);
+            await _dataContext.SaveChangesAsync();
+            return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto login)
+        {
+            var user = await _dataContext.User.SingleAsync(x => x.UserName == login.Name.ToLower());
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedPasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(login.Password));
+
+            for(int i = 0; i < computedPasswordHash.Length; i++)
+            {
+                if (user.PasswordHash[i] != computedPasswordHash[i])
+                {
+                    return Unauthorized();
+                }
+            }
+
+            return new UserDto
+            {
+                UserName = user.UserName,
+                Token = tokenService.CreateToken(user)
+            };
+        }
+    }
+}
---
+++ DataingApp/API/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using API.Data;
+using API.Entities;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Dfareporting.v4;
+using Google.Apis.Util.Store;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class UsersController : BaseApiController
+    {
+        public readonly DataContext _dataContext;
+
+        public UsersController(DataContext dataContext)
+        {
+            this._dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<List<AppUser>> Get()
+        {
+            var users = await _dataContext.User.ToListAsync();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _dataContext.User.FindAsync(id);
+
+            return Ok(user);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddUser(AppUser user)
+        {
+            var newUser = new AppUser
+            {
+                UserName = user.UserName,
+            };
+             await _dataContext.User.AddAsync(newUser);
+            var result =  await _dataContext.SaveChangesAsync();
+
+            return Ok(result);
+        }
+
+    }
+}
